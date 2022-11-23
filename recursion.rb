3021e279d8349601ca0e09f0166b2a7a98a227d7
Charles Tyra <charlestyra@macbook-pro.myfiosgateway.com>
2022-11-23 16:52:46 -0500

    stuck on permutations and merge  -- mostly just collaborative writing at this point

--- recursion.rb
+++ recursion.rb
@@ -79,6 +79,7 @@ end
 p iter_fib(14)
 p iter_fib(8)
 
+
 def bsearch(array, target)
     return nil if array.length == 1 && array[0] != target
 
@@ -106,28 +107,60 @@ p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
 p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
 p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
 
+
 def merge(array)
     return array if array.length == 1
 
     res_array = []
-
     mid = array.length / 2
     left = merge(array[0...mid])
     right = merge(array[mid..-1])
 
     while left.length > 0 || right.length > 0
-        if left[0] < right[0]
-            res_array << left.shift
-        elsif left[0] > right[0]
-           res_array << right.shift
-        elsif left.length == 0 
+        if left.length == 0 
             res_array << right.shift
-        else 
+        elsif right.length == 0
             res_array << left.shift
+        elsif left[0] < right[0]
+            res_array << left.shift
+        else 
+            res_array << right.shift
         end
     end
     res_array
 end
-
-
 p merge([5,2,7,3,1,9])
+
+
+# def subsets(array)
+#     array.each do |ele|
+#         if subsets(array) == 0
+#             return array if array.length == 0
+#         end
+#     end
+
+# end
+# subsets([]) # => [[]]
+# subsets([1]) # => [[], [1]]
+# subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+# subsets([1, 2, 3]) # => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+
+def permutations(array)
+    return array[0] if array.length == 1
+
+    perm_arr = []
+    prv_1 = (array.length + 1) % array.length * -1
+    prv_2 = (array.length + 2) % array.length * -1
+    array.each do |ele|
+        (0...array.length).each do |i|
+            if ele != array[i]
+                perm_arr << permutations([array[i - prv_1], array[i - prv_2]])
+            end
+        end
+    end
+    perm_arr
+end
+p permutations([1, 2, 3]) # => [[1, 2, 3], [1, 3, 2],
+                        #     [2, 1, 3], [2, 3, 1],
+                        #     [3, 1, 2], [3, 2, 1]]
